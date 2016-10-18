e3311e74df93696dabaa73865f35ba91dc2a3646
ethanmchung <echung2018@pingry.org>
2016-10-17 22:36:13 -0400

    added javadoc comments. fixed formatting

--- charAt.java
+++ charAt.java
@@ -3,18 +3,29 @@
 
 //parameter − index − Index of the character to be returned
 //returns - a char at the specified index in the string
-public class charAt {
 
-   public static void main(String args[]) {
-      String str = "Compiler";
-      char one = str.charAt(0);
-      char two = str.charAt(1);
-      char three = str.charAt(1);
-      char four = str.charAt(5);
-      
-      System.out.println(one);
-      System.out.println(two);
-      System.out.println(three);
-      System.out.println(four);
-   }
+/**
+ * This class demonstrates how the method "charAt" works
+ * @author Ethan Chung
+ * @version 1.0
+ */
+public class charAt
+{
+	/**
+      * Main method uses charAt to spell out "cool" by using letters from the word "compiler"
+      * @param args Standard array of commandline String arguments. Not used in this program.
+      */  
+	public static void main(String args[])
+	{
+		String str = "Compiler";
+		char one = str.charAt(0);
+		char two = str.charAt(1);
+		char three = str.charAt(1);
+		char four = str.charAt(5);
+		
+		System.out.print(one);
+		System.out.print(two);
+		System.out.print(three);
+		System.out.println(four);
+	}
 }
